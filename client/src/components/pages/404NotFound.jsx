2bfc49951bb4f1d22fc1bfe9030e54722367d8e2
Josh Schmitz <josh.schmitz1@gmail.com>
2025-02-05 12:15:05 -0600

    add Empty and home button to 404 Page

--- client/src/components/pages/404NotFound.jsx
+++ client/src/components/pages/404NotFound.jsx
@@ -1,4 +1,23 @@
+import { useNavigate } from 'react-router-dom';
+
+// import components
+import { Empty, Button, Typography } from 'antd';
+
 const NotFound = () => {
-  return <div>NotFound</div>;
+  const navigate = useNavigate();
+
+  const onClick = () => {
+    navigate('/');
+  };
+
+  return (
+    <Empty
+      description={<Typography.Text>Oops! 404 Page not found.</Typography.Text>}
+    >
+      <Button type='primary' onClick={onClick}>
+        Go Home
+      </Button>
+    </Empty>
+  );
 };
 export default NotFound;
