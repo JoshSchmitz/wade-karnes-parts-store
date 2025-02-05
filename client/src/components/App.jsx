a8f087fcf35a5d9957e86b1ac3c0a85bd59b4022
Josh Schmitz <josh.schmitz1@gmail.com>
2025-02-04 19:51:53 -0600

    add app routes

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,12 +1,24 @@
-import '../css/App.css';
-
 // import components
-import { Button } from 'antd';
+import { Outlet } from 'react-router-dom';
+import { Layout } from 'antd';
+
+const { Header, Content, Footer } = Layout;
 
 function App() {
   return (
     <div className='App'>
-      <Button type='primary'>Button</Button>
+      <Layout>
+        <Header>
+          <h3>Header</h3>
+        </Header>
+        <Content>
+          <h3>Content</h3>
+          <Outlet />
+        </Content>
+        <Footer>
+          <h3>Footer</h3>
+        </Footer>
+      </Layout>
     </div>
   );
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,40 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './css/index.css';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
+
+// import css
+import './css/main.min.css';
+
+// import components
 import App from './components/App.jsx';
+
+// import pages
+import HomePage from './components/pages/HomePage.jsx';
+import ContactPage from './components/pages/ContactPage.jsx';
+import ProductPage from './components/pages/ProductPage.jsx';
+import ProfilePage from './components/pages/ProfilePage.jsx';
+import NotFoundPage from './components/pages/404NotFound.jsx';
+
+// react router
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route route='/' element={<App />}>
+      <Route index={true} path='/' element={<HomePage />} />
+      <Route path='/product/:id' element={<ProductPage />} />
+      <Route path='/contact' element={<ContactPage />} />
+      <Route path='/profile' element={<ProfilePage />} />
+      <Route path='*' element={<NotFoundPage />} />
+    </Route>
+  )
+);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
